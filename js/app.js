b2e4e31ac451953c8c34b81befdc467f5bf96ff9
eembree <eric-embree@uiowa.edu>
2015-10-30 12:43:28 -0500

    Added a bunch of directives

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 angular.module('myApp', ['imFormComponents', 'panel'])
 
 .controller('imFormCtrl', ['$scope', function($scope){
-	$scope.dropdata = angular.fromJson('{"name":"villain","display":"Best Villain","required":true,"selected":"","options":[{"value":"","text":"-- Select --"},{"value":"1","text":"Darth Vader"},{"value":"2","text":"The Joker"},{"value":"3","text":"Doctor Doom"}]}');
-	$scope.textdata = angular.fromJson('{"name":"lastname","display":"Peron\'s Last Name","required":true}');
+	$scope.dropdata = angular.fromJson('{"name":"villain","label":"Best Villain","required":true,"selected":"","options":[{"value":"","text":"-- Select --"},{"value":"1","text":"Darth Vader"},{"value":"2","text":"The Joker"},{"value":"3","text":"Doctor Doom"}]}');
+	$scope.textdata = angular.fromJson('{"name":"lastname","label":"Peron\'s Last Name","required":true}');
+	$scope.textarea = angular.fromJson('{"name":"adventure","label":"Describe your greatest adventure","required":true}');
+	$scope.checkboxdata = angular.fromJson('{"name":"vehicle","label":"Do you ride a bike?","display":"Yes, I ride a bike","value":"y","checked":true}');
+	$scope.checkboxlistdata = angular.fromJson('{"label":"Which of the following colors do you like?","options": [{"name":"realcolor","display":"Red","value":"red","checked":false},{"name":"realcolor","display":"Blue","value":"blue","checked":true},{"name":"fakecolor","display":"Moodlely","value":"Moodlely","checked":false}]}');
+	$scope.radiodata= angular.fromJson('{"name":"cakeorpie","label":"Cake or pie?","options":[{"display":"No Preference","value":"nopref","checked":true},{"display":"Cake","value":"cake","checked":false},{"display":"Pie","value":"pie","checked":false}]}');
+	console.log($scope.checkboxlistdata)
 }])
--- js/im_form_components/im-form-components.js
+++ js/im_form_components/im-form-components.js
@@ -12,7 +12,7 @@ angular.module('imFormComponents', [])
 	};
 })
 
-
+// Represent a textbox expecting JSON in a specific format
 .directive('imTextbox', function(){
 	return {
 		restrict: 'E',
@@ -20,5 +20,53 @@ angular.module('imFormComponents', [])
 			data: '=',
 		},
 		templateUrl: 'im_form_templates/imTextbox.html'
+	};
+})
+
+
+// Represent a textarea expecting JSON in a specific format
+.directive('imTextarea', function(){
+	return {
+		restrict: 'E',
+		scope: {
+			data: '=',
+		},
+		templateUrl: 'im_form_templates/imTextarea.html'
+	};
+})
+
+
+// Represent a checkbox expecting JSON in a specific format
+.directive('imCheckbox', function(){
+	return {
+		restrict: 'E',
+		scope: {
+			data: '=',
+		},
+		templateUrl: 'im_form_templates/imCheckbox.html'
+	};
+})
+
+
+// Represent a list of checkbox items with a single header expecting JSON in a specific format
+.directive('imCheckboxList', function(){
+	return {
+		restrict: 'E',
+		scope: {
+			data: '=',
+		},
+		templateUrl: 'im_form_templates/imCheckboxList.html'
+	};
+})
+
+
+// Represent a radio button group with a single header expecting JSON in a specific format
+.directive('imRadio', function(){
+	return {
+		restrict: 'E',
+		scope: {
+			data: '=',
+		},
+		templateUrl: 'im_form_templates/imRadio.html'
 	};
 })
